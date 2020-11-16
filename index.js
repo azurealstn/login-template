7076fc733843eed8c071ee35bf0e17011d1e3783
azurealstn <azurealstn@naver.com>
2020-11-17 01:37:54 +0900

    bcrypt & create Token

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5000;
 
 const { User } = require('./models/User');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const key = require('./config/key');
 
@@ -13,7 +14,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //application/json
 app.use(bodyParser.json());
 
+//cookie-parser 사용
+app.use(cookieParser());
+
 const mongoose = require('mongoose');
+const { response } = require('express');
 mongoose
   .connect(key.mongoURI, {
     useNewUrlParser: true,
@@ -35,6 +40,37 @@ app.post('/register', (request, response) => {
   user.save((error, userInfo) => {
     if (error) return response.json({ saveSuccess: false, error });
     return response.status(200).json({ saveSuccess: true });
+  });
+});
+
+app.post('/login', (request, response) => {
+  // DB에서 찾기
+  User.findOne({ email: request.body.email }, (error, user) => {
+    if (!user) {
+      return response.json({
+        loginSuccess: false,
+        message: '해당하는 이메일의 유저가 없습니다.',
+      });
+    }
+    //비밀번호 확인하기
+    user.confirmPassword(request.body.password, (error, isSame) => {
+      if (!isSame)
+        return response.json({
+          loginSuccess: false,
+          message: '패스워드가 틀렸습니다.',
+        });
+
+      //비밀번호가 맞다면 토큰 생성하기
+      user.createToken((err, user) => {
+        if (err) return response.status(400).send(err);
+
+        //쿠키에 토큰 저장
+        response
+          .cookie('x_auth', user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
   });
 });
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   userName: {
@@ -26,6 +29,48 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+// mongodb save() 하기 전에 password 암호화(bcrypt)
+// next(): 바로 mongodb save() 로 이동
+// user.password === myPlaintextPassword: 암호화가 아닌 사용자가 입력한 password
+userSchema.pre('save', function (next) {
+  let user = this; //userSchema
+
+  //password가 변경될 때에만 암호화를 해준다.
+  //만약 password가 아닌 email변경시 다시 암호화 해주기 때문에 error 발생
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash; //hash로 변경
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+//myPlaintextPassword와 bcrypt password가 서로 같은지 확인
+userSchema.methods.confirmPassword = function (myPlaintextPassword, fn) {
+  bcrypt.compare(myPlaintextPassword, this.password, function (err, isSame) {
+    if (err) return fn(err);
+    fn(null, isSame);
+  });
+};
+
+//토큰 생성
+userSchema.methods.createToken = function (fn) {
+  let user = this;
+  let token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return fn(err);
+    fn(null, user);
+  });
+};
 
 const User = mongoose.model('User', userSchema);
 
